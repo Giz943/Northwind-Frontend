43c987e16dd0d68def227d2cac09db8438817635
Giz943 <gizemcinar23@gmail.com>
2021-03-31 15:32:34 +0300

    Bilgiler güncellendi

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { CategoryComponent } from './components/category/category.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    NaviComponent,
+    CategoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
